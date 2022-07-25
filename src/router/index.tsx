a5e0bc94b407364b990ac9db094457b8d96b7cf6
Ubeom Kim <kimub1204@gmail.com>
2022-07-25 18:17:09 +0900

    feat: Todo, Settings 페이지 라우팅 기능 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 
 import Layout from '@/components/Layout';
 import Home from '@/pages/Home';
+import Settings from '@/pages/Settings';
+import Todo from '@/pages/Todo';
 import * as S from '@/router/style';
 
 const Router = () => {
@@ -10,6 +12,8 @@ const Router = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path='/home' element={<Home />} />
+          <Route path='/todo' element={<Todo />} />
+          <Route path='/settings' element={<Settings />} />
         </Route>
       </Routes>
     </S.Container>
