11eb9ec5dd3b9df1eda1c4954c39a756c554921a
Ubeom Kim <kimub1204@gmail.com>
2022-07-25 18:15:10 +0900

    style: Home 페이지 컴포넌트 반응형 크기 수정

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.main`
   padding: 2.222222222222222%;
-  width: 100%;
+  width: 82.22222222222222%;
   height: 100%;
   background-color: ${({ theme: { colors } }) => colors.gray[400]};
 `;
