9a8c7daa89e9f1d4eae0c35554568ce0e9254faa
Ubeom Kim <kimub1204@gmail.com>
2022-07-28 17:19:13 +0900

    feat: covid api 데이터 파싱

--- src/components/Covid/CovidStatus/index.tsx
+++ src/components/Covid/CovidStatus/index.tsx
@@ -1,17 +1,45 @@
+import * as S from './style';
+import { ICovidStatusItem } from './types';
+
 import useCovidStatus from '@/hooks/useCovidStatus';
+import COVID_STATUS_MOCK from '@/mocks/covidStatus';
+
+const CovidStatusItem = ({ data, count }: ICovidStatusItem) => {
+  return (
+    <S.StatusItem>
+      {data.icon}
+      <strong>{count}</strong>
+      <h3>{data.title}</h3>
+    </S.StatusItem>
+  );
+};
 
 const CovidStatus = () => {
   const { isLoading, data } = useCovidStatus();
 
   if (isLoading) return <div>Loading...</div>;
 
-  if (data)
+  if (data) {
+    const covidApiData = [
+      data.cnt_confirmations,
+      data.cnt_severe_symptoms,
+      data.cnt_hospitalizations,
+      data.cnt_deaths,
+    ];
+
+    const statusItems = COVID_STATUS_MOCK.map((data, index) => (
+      <CovidStatusItem key={index} count={covidApiData[index]} {...{ data }} />
+    ));
+
     return (
-      <div>
-        {data.cnt_confirmations} {data.cnt_severe_symptoms}
-        {data.cnt_hospitalizations} {data.cnt_deaths}
-      </div>
+      <section>
+        {/* heading 추가하고 readonly */}
+        <S.StatusList justifyContent='center' alignItems='center'>
+          {statusItems}
+        </S.StatusList>
+      </section>
     );
+  }
 
   return <div>Error</div>;
 };
