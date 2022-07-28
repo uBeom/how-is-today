c3001e3b26f19c6ac17fffadfe8985bdeb1a71ce
Ubeom Kim <kimub1204@gmail.com>
2022-07-28 17:13:20 +0900

    style: GNB 관련 아이콘들 height 속성 추가

--- src/assets/icons/Header/style.ts
+++ src/assets/icons/Header/style.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Svg = styled.svg`
   margin-right: 13.38912133891213%;
   width: 10.0418410041841%;
+  height: 10.0418410041841%;
 
   &:hover {
     fill: ${({ theme: { colors } }) => colors.blue[600]};
