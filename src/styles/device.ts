bcb1df7c6e120b2c16a647d83d104ac2d6acb108
Ubeom Kim <kimub1204@gmail.com>
2022-07-30 18:44:55 +0900

    style: 미디어쿼리 구조 추가

---
+++ src/styles/device.ts
@@ -0,0 +1,11 @@
+const sizes = {
+  mobile: '375px',
+  tablet: '768px',
+  desktop: '1440px',
+};
+
+export const devices = {
+  mobile: `(min-width: ${sizes.mobile})`,
+  tablet: `(min-width: ${sizes.tablet})`,
+  desktop: `(min-width: ${sizes.desktop})`,
+};
