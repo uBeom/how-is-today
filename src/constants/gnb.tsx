99df188bb952067cac7f76410246fc521e380002
uBeom <kimub1204@gmail.com>
2022-07-31 16:51:55 +0900

    rename: GNB_MOCK -> GNB

--- src/constants/gnb.tsx
+++ src/constants/gnb.tsx
@@ -1,9 +1,7 @@
 import Icon from '@/components/common/Icons';
 
-const GNB_MOCK = [
+export const GNB = [
   { name: '홈', src: '/home', icon: <Icon name='home' size='sm' /> },
   { name: '투두', src: '/todo', icon: <Icon name='todo' size='sm' /> },
   { name: '설정', src: '/settings', icon: <Icon name='settings' size='sm' /> },
 ];
-
-export default GNB_MOCK;
