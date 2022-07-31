55b52fbb272d8fd3890e6ade777829939404d4f8
uBeom <kimub1204@gmail.com>
2022-07-31 12:19:33 +0900

    style: 반응형 pc 레이아웃 변경
    flex -> grid로 레이아웃 변경

--- src/router/style.ts
+++ src/router/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 144rem;
+  max-width: 146.4rem;
   max-height: 102.4rem;
   width: 100%;
   height: 100%;
