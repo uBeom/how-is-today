b511b78e68c40d72fa694a89b51ea6e38df2e5d8
uBeom <kimub1204@gmail.com>
2022-08-03 20:20:56 +0900

    feat: useFetchCovidChart 커스텀훅 추가
    CoivdChart 관련 api 요청 기능

---
+++ src/hooks/useFetchCovidChart.ts
@@ -0,0 +1,45 @@
+import { useQuery } from '@tanstack/react-query';
+
+export interface IUseCoivdChart {
+  cnt3: string;
+  cnt4: string;
+  cnt1: string;
+  cnt2: string;
+  cnt7: string;
+  cnt8: string;
+  cnt5: string;
+  cnt6: string;
+  rate4: string;
+  rate5: string;
+  rate6: string;
+  rate7: string;
+  rate1: string;
+  rate2: string;
+  rate3: string;
+  rate8: string;
+  mmdd4: string;
+  mmdd5: string;
+  mmdd6: string;
+  mmdd7: string;
+  mmdd1: string;
+  mmdd2: string;
+  mmdd3: string;
+  mmdd8: string;
+  mmddhh: string;
+}
+
+const COVID_CHART_URL = `http://apis.data.go.kr/1790387/covid19CurrentStatusConfirmations/covid19CurrentStatusConfirmationsJson?ServiceKey=${process.env.REACT_APP_CORONA_KEY}`;
+
+const fetchCovidChart = async (): Promise<IUseCoivdChart> => {
+  const res = await fetch(COVID_CHART_URL);
+  const {
+    response: { result },
+  } = await res.json();
+
+  return result[0];
+};
+
+const useFetchCovidChart = () =>
+  useQuery<IUseCoivdChart, Error>(['covidchart'], fetchCovidChart);
+
+export default useFetchCovidChart;
