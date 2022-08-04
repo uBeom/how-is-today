d2f780de67d954e6bfa5eff2ede7dff82180ee76
Ubeom Kim <kimub1204@gmail.com>
2022-08-04 12:39:21 +0900

    init: no-else-return 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -83,6 +83,7 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react/no-array-index-key': 'off',
     'no-shadow': 'off',
+    'no-else-return': 'off',
   },
   env: {
     browser: true,
