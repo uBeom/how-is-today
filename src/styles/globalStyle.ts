5e474268fa8d9f5be4c59758b1965954e62d2148
Ubeom Kim <kimub1204@gmail.com>
2022-08-04 17:23:26 +0900

    init: 폰트 설정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,20 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
-import fonts from './fonts';
-
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  ${fonts}
 
   html {
+    font-family: 'Noto Sans KR';
     font-size: 62.5% !important;
     background-color: ${({ theme: { colors } }) => colors.gray[200]};
   }
 
   * {
     box-sizing: border-box !important;
-    font-family: Noto Sans KR, sans-serif;
     color: ${({ theme: { colors } }) => colors.gray[200]};
   }
 
