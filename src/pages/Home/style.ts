d946527595de8381f6d9274c7c83a95948243b09
uBeom <kimub1204@gmail.com>
2022-08-07 21:15:46 +0900

    style: Home 스타일 레이아웃 세로 변경

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -12,7 +12,7 @@ export const Container = styled.main`
     width: 120.8rem;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(5, 1fr);
+    grid-template-rows: repeat(3, 1fr);
     gap: 3.2rem;
   }
 `;
