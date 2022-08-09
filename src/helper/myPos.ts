e935b2ca82db004d2cdb16d91ea94b77d224b384
Ubeom Kim <kimub1204@gmail.com>
2022-08-09 22:22:29 +0900

    remove: 불필요한 myPos 클래스 제거

--- src/helper/myPos.ts
+++
@@ -1,8 +0,0 @@
-export class MyPos {
-  static getPos() {
-    navigator.geolocation.getCurrentPosition(pos => {
-      console.log(`위도: ${pos.coords.latitude}`);
-      console.log(`경도: ${pos.coords.longitude}`);
-    });
-  }
-}
